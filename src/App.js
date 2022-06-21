9059af80c76000a9fe303f041412cb9cf92adadb
nedosika <nedosika@gmail.com>
2022-06-21 18:31:07 +0300

    create footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ComplexTable from "./components/ComplexTable"
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div>
+    <div className={styles.root}>
       <ComplexTable/>
     </div>
   );
