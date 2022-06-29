af1b10cdcbafd51b809e209452d738799aca7bee
nedosika <nedosika@gmail.com>
2022-06-29 23:23:57 +0300

    add background color when selected and hover

--- src/components/CheckedTable/CheckedRow.js
+++ src/components/CheckedTable/CheckedRow.js
@@ -3,7 +3,14 @@ import Row from "../Table/Main/Row/TableRow";
 import CheckBoxSelection from "./CheckBoxSelection";
 
 const CheckedRow = ({isChecked, toggle, isShow, row, ...props}) =>
-    <Row {...props}>
+    <Row
+        {...props}
+        sx={isChecked(row) &&
+            {
+                backgroundColor: 'rgba(25, 118, 210, 0.08)'
+            }
+        }
+    >
         {
             isShow &&
             <CheckBoxSelection isChecked={isChecked(row)} toggle={() => toggle(row)}/>
--- src/components/Table/Main/Row/TableRow.js
+++ src/components/Table/Main/Row/TableRow.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from "./TableRow.module.scss";
 
-const TableRow = ({children, items = [], renderItem}) => {
+const TableRow = ({children, items = [], renderItem, sx}) => {
     return (
-        <div className={styles.root}>
+        <div className={styles.root} style={{...sx}}>
             {children}
             {items.map(renderItem)}
         </div>
