c6030b20ce808101b188dd3b4e9c6f9cafc9ed2f
nedosika <nedosika@gmail.com>
2022-07-31 13:51:52 +0300

    fix MUI warning

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,28 +4,32 @@ import Tooltip from "@mui/material/Tooltip";
 import {useTableProps} from "../ComplexTable/ComplexTable";
 
 const COLORS = {
-  disable: "lightgrey",
-  primary: "rgb(25, 118, 210)",
+    disable: "lightgrey",
+    primary: "rgb(25, 118, 210)",
 };
 
-const Button = ({ onClick, icon, title, hint = '', disabled = false, sx }) => {
-  const { selectedRows = [], rows } = useTableProps();
-  const isDisabled = typeof disabled === "boolean" ? disabled : disabled({selectedRows, rows});
-  const color = isDisabled ? COLORS.disable : COLORS.primary;
+const Button = ({onClick, icon, title, hint = '', disabled = false, sx}) => {
+    const {selectedRows = [], rows} = useTableProps();
+    const isDisabled = typeof disabled === "boolean" ? disabled : disabled({selectedRows, rows});
+    const color = isDisabled ? COLORS.disable : COLORS.primary;
 
-  return (
-    <Tooltip title={typeof hint === "string" ? hint : hint({selectedRows, rows})}>
-      <button
-        className={styles.root}
-        style={{ color, ...sx }}
-        onClick={onClick}
-        disabled={isDisabled}
+    return (
+      <Tooltip
+        title={typeof hint === "string" ? hint : hint({ selectedRows, rows })}
       >
-        <span>{icon({ color })}</span>
-        <span className={styles.title}>{title}</span>
-      </button>
-    </Tooltip>
-  );
+        <span>
+          <button
+            className={styles.root}
+            style={{ color, ...sx }}
+            onClick={onClick}
+            disabled={isDisabled}
+          >
+            <span>{icon({ color })}</span>
+            <span className={styles.title}>{title}</span>
+          </button>
+        </span>
+      </Tooltip>
+    );
 };
 
 export default Button;
